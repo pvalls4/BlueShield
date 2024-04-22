21a1d2da75a66aca4e807703815746817dc1eaa7
Carlos Alberto <dalbert9615@gmail.com>
2024-04-22 11:17:09 +0200

    Add AgenteDAO n CiudadanoDAO

---
+++ src/main/java/DAO/AgenteDAO.java
@@ -0,0 +1,141 @@
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.DTO.*;
+
+public class AgenteDAO {
+   
+    private static final String SQL_SELECT_ALL = "SELECT * FROM agentes";
+    private static final String SQL_SELECT = "SELECT * FROM agentes WHERE placa = ?";
+    private static final String SQL_INSERT = "INSERT INTO agentes(dniAgente, password, imagen) VALUES(?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE agentes SET dniAgente=?, password=?, imagen=?, WHERE placa = ?";
+    private static final String SQL_DELETE = "DELETE FROM agentes WHERE placa=?";
+
+    private AgenteDTO fromResultSet(ResultSet rs) throws SQLException {
+        int placa = rs.getInt("placa");
+        
+        String dniAgente = rs.getString("dniAgente");
+        CiudadanoDTO ciudadano = new CiudadanoDAO().select(dniAgente);
+
+        String password = rs.getString("password");
+        String imagen = rs.getString("imagen");
+
+        AgenteDTO agente = new AgenteDTO(placa,ciudadano, password, imagen);
+
+        return agente;
+    }
+
+    public List<AgenteDTO> selectAll() throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        AgenteDTO agente = null;
+        List<AgenteDTO> agentes = new ArrayList<AgenteDTO>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT_ALL);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                agente = fromResultSet(rs);
+                if (agente != null) {
+                    agentes.add(agente);
+                }
+            }
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+        }
+
+        return agentes;
+    }
+
+    public AgenteDTO select(int placa) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        AgenteDTO agente = null;
+
+        try {
+            conn = Conexion.getConnection();
+            if (conn != null) {
+                stmt = conn.prepareStatement(SQL_SELECT);
+                stmt.setInt(1, placa);
+                rs = stmt.executeQuery();
+                if (rs.next()) {
+                    agente = fromResultSet(rs);
+                }
+            }
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+        }
+
+        return agente;
+    }
+
+    public int insert(AgenteDTO agente) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, agente.getCiudadano().getDni());
+            stmt.setString(2, agente.getPassword());
+            stmt.setString(3, agente.getEnlaceFotografico());
+
+            System.out.println("ejecutando query:" + SQL_INSERT);
+            rows = stmt.executeUpdate();
+            System.out.println("Registros afectados:" + rows);
+        } finally {
+            Conexion.close(stmt);
+        }
+
+        return rows;
+    }
+
+    public int update(AgenteDTO agente) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            System.out.println("ejecutando query: " + SQL_UPDATE);
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, agente.getCiudadano().getDni());
+            stmt.setString(2, agente.getPassword());
+            stmt.setString(3, agente.getEnlaceFotografico());
+
+            rows = stmt.executeUpdate();
+            System.out.println("Registros actualizado:" + rows);
+
+        } finally {
+            Conexion.close(stmt);
+        }
+
+        return rows;
+    }
+
+    public int delete(AgenteDTO agente) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            System.out.println("Ejecutando query:" + SQL_DELETE);
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, agente.getPlaca());
+            rows = stmt.executeUpdate();
+            System.out.println("Registros eliminados:" + rows);
+        } finally {
+            Conexion.close(stmt);
+        }
+
+        return rows;
+    }
+}
---
+++ src/main/java/DAO/CiudadanoDAO.java
@@ -0,0 +1,159 @@
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.DTO.*;
+
+public class CiudadanoDAO {
+
+    private static final String SQL_SELECT_ALL = "SELECT * FROM ciudadanos";
+    private static final String SQL_SELECT = "SELECT * FROM ciudadanos WHERE dni = ?";
+    private static final String SQL_INSERT = "INSERT INTO ciudadanos(dni, nombre, apellidos, fecha_nacimiento, telefono, email, isDeceased, imagen, idDirecciones) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE ciudadanos SET nombre=?, apellidos=?, fecha_nacimiento=?, telefono=?, email=?, isDeceased=?, imagen=?, idDirecciones=? WHERE dni = ?";
+    private static final String SQL_DELETE = "DELETE FROM ciudadnos WHERE dni=?";
+
+    private CiudadanoDTO fromResultSet(ResultSet rs) throws SQLException {
+        String dni = rs.getString("dni");
+        String nombre = rs.getString("nombre");
+        String apellidos = rs.getString("apellidos");
+        Date fecha_nacimiento = rs.getDate("fecha_nacimiento");
+        int telefono = rs.getInt("telefono");
+        String email = rs.getString("email");
+        boolean isDeceased = rs.getBoolean("isDeceased");
+        String imagen = rs.getString("imagen");
+
+        int idDirecciones = rs.getInt("idDirecciones");
+        DireccionDTO direccion = new DireccionDAO().select(idDirecciones);
+
+        CiudadanoDTO ciudadano = new CiudadanoDTO(dni, nombre, apellidos, fecha_nacimiento, telefono, email, isDeceased, direccion, imagen);
+
+        return ciudadano;
+    }
+
+    public List<CiudadanoDTO> selectAll() throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        CiudadanoDTO ciudadano = null;
+        List<CiudadanoDTO> ciudadanos = new ArrayList<CiudadanoDTO>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT_ALL);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                ciudadano = fromResultSet(rs);
+                if (ciudadano != null) {
+                    ciudadanos.add(ciudadano);
+                }
+            }
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+        }
+
+        return ciudadanos;
+    }
+
+    public CiudadanoDTO select(String dni) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        CiudadanoDTO ciudadano = null;
+
+        try {
+            conn = Conexion.getConnection();
+            if (conn != null) {
+                stmt = conn.prepareStatement(SQL_SELECT);
+                stmt.setString(1, dni);
+                rs = stmt.executeQuery();
+                if (rs.next()) {
+                    ciudadano = fromResultSet(rs);
+                }
+            }
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+        }
+
+        return ciudadano;
+    }
+
+    public int insert(CiudadanoDTO ciudadano) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, ciudadano.getDni());
+            stmt.setString(2, ciudadano.getNombre());
+            stmt.setString(3, ciudadano.getApellidos());
+            stmt.setDate(4, ciudadano.getFecha_nacimiento());
+            stmt.setInt(5, ciudadano.getTelefono());
+            stmt.setString(6, ciudadano.getEmail());
+            stmt.setBoolean(7, ciudadano.isIsDeceased());
+            stmt.setString(8, ciudadano.getEnlaceFotografico());
+            stmt.setInt(9, ciudadano.getDireccion().getId());
+
+            System.out.println("ejecutando query:" + SQL_INSERT);
+            rows = stmt.executeUpdate();
+            System.out.println("Registros afectados:" + rows);
+        } finally {
+            Conexion.close(stmt);
+        }
+
+        return rows;
+    }
+
+    public int update(CiudadanoDTO ciudadano) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            System.out.println("ejecutando query: " + SQL_UPDATE);
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, ciudadano.getNombre());
+            stmt.setString(2, ciudadano.getApellidos());
+            stmt.setDate(3, ciudadano.getFecha_nacimiento());
+            stmt.setInt(4, ciudadano.getTelefono());
+            stmt.setString(5, ciudadano.getEmail());
+            stmt.setBoolean(6, ciudadano.isIsDeceased());
+            stmt.setString(7, ciudadano.getEnlaceFotografico());
+            stmt.setInt(8, ciudadano.getDireccion().getId());
+
+            //set del id a cambiar
+            stmt.setString(9, ciudadano.getDni());
+
+            rows = stmt.executeUpdate();
+            System.out.println("Registros actualizado:" + rows);
+
+        } finally {
+            Conexion.close(stmt);
+        }
+
+        return rows;
+    }
+
+    public int delete(CiudadanoDTO ciudadano) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            System.out.println("Ejecutando query:" + SQL_DELETE);
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setString(1, ciudadano.getDni());
+            rows = stmt.executeUpdate();
+            System.out.println("Registros eliminados:" + rows);
+        } finally {
+            Conexion.close(stmt);
+        }
+
+        return rows;
+    }
+}
