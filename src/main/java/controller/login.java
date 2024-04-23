15a432943559f775fa6750b7d4c80f3a7f3a31f0
pvalls4 <valls.vilalta.4@gmail.com>
2024-04-23 10:23:39 +0200

    Dashboard

--- src/main/java/controller/login.java
+++ src/main/java/controller/login.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
+import DAO.AgenteDAO;
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
@@ -11,6 +8,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.DTO.AgenteDTO;
 
 /**
  *
@@ -30,9 +31,44 @@ public class login extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-             RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+             RequestDispatcher dispatcher = request.getRequestDispatcher("/view/login.jsp");
              dispatcher.forward(request, response);
     }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Retrieve username and password parameters from the request
+        int placa = Integer.parseInt(request.getParameter("placa"));
+        String pwd = request.getParameter("pwd");
+
+        // Perform authentication (e.g., check against a database)
+        boolean isAuthenticated = authenticate(placa, pwd);
+
+        if (isAuthenticated) {
+            // If authentication succeeds, forward the request to the login.jsp page
+            request.getRequestDispatcher("/view/dashboard.jsp").forward(request, response);
+        } else {
+            // If authentication fails, display an error message
+            request.setAttribute("errorMessage", "Invalid username or password");
+            request.getRequestDispatcher("/view/login.jsp").forward(request, response);
+}
+            
+    }
+    private boolean authenticate(int placa, String pwd) 
+        throws ServletException, IOException {
+            AgenteDAO dao = new AgenteDAO();
+            try {
+                System.out.println("#############################################");
+                System.out.println(placa);
+                System.out.println(pwd);
+                AgenteDTO agente = dao.select(placa);
+                System.out.println(agente);
+            } catch (SQLException ex) {
+                Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return placa==142 && pwd.equals("baylor");
+        }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -57,11 +93,7 @@ public class login extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+    
 
     /**
      * Returns a short description of the servlet.
@@ -72,5 +104,6 @@ public class login extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
+    
 }
+    
---
+++ src/main/webapp/view/dashboard.jsp
@@ -0,0 +1,12 @@
+<%@ include file="./header.jsp" %>
+<div class="d-flex justify-content-center">
+    <img id="ciudadanos" src="./images/ciudadanos.png" class="dash-option m-5" onclick="redirigir(this.id)">
+    <img id="vehiculos" src="./images/vehiculos.png" class="dash-option m-5" onclick="redirigir(this.id)">
+    <img id="multas" src="./images/multas.png" class="dash-option m-5" onclick="redirigir(this.id)">
+</div>
+<script>
+    function redirigir(id) {
+    window.location.href = id;
+    }
+</script>
+<%@ include file="./footer.jsp" %>
