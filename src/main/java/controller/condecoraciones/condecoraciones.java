fa0591e6dd9b1334cf80700846812cb099018671
pvalls <valls.vilalta.4@gmail.com>
2024-04-29 00:50:47 +0200

    Avances en condecoraciones. Mostrar todas + mostrar agentes que tengan una condecoracion concreta

--- src/main/java/controller/condecoraciones/condecoraciones.java
+++ src/main/java/controller/condecoraciones/condecoraciones.java
@@ -1,84 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller.condecoraciones;
 
+import DAO.CiudadanoDAO;
+import DAO.CondecoracionDAO;
+import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.DTO.CiudadanoDTO;
+import model.DTO.CondecoracionDTO;
 
-/**
- *
- * @author Mati
- */
 @WebServlet(name = "condecoraciones", urlPatterns = {"/condecoraciones"})
 public class condecoraciones extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet condecoraciones</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet condecoraciones at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            HttpSession session = request.getSession(false);
+
+            if (session != null && session.getAttribute("username") != null) {
+                request.setAttribute("username", session.getAttribute("username"));
+                response.setContentType("text/html;charset=UTF-8");
+                CondecoracionDAO dao = new CondecoracionDAO();
+                List<CondecoracionDTO> listaCondecoraciones = dao.selectAll();
+                request.setAttribute("listaCondecoraciones", listaCondecoraciones);
+                RequestDispatcher rd = request.getRequestDispatcher("./view/condecoraciones/condecoraciones.jsp");
+                rd.forward(request, response);
+            } else {
+                response.sendRedirect("login");
+            } 
+        } catch (SQLException ex) {
+            Logger.getLogger(condecoraciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+                String dni = request.getParameter("dni");
+                response.sendRedirect("ciudadano?id=" + dni);
+            }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
