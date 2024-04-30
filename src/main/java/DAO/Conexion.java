334022504032655dfce51ca6b167fe23cc3f1051
pvalls <valls.vilalta.4@gmail.com>
2024-04-30 21:27:55 +0200

    Parche para trabajar en local

--- src/main/java/DAO/Conexion.java
+++ src/main/java/DAO/Conexion.java
@@ -11,27 +11,27 @@ import java.util.logging.Logger;
 
 public class Conexion {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String connectionString = System.getenv("AZURE_MYSQL_CONNECTIONSTRING");
-//    private static final Properties properties = new Properties();
+//    private static final String connectionString = System.getenv("AZURE_MYSQL_CONNECTIONSTRING");
+    private static final Properties properties = new Properties();
 
-//    static {
-//        try (InputStream inputStream = Conexion.class.getClassLoader().getResourceAsStream("./config/config.properties")) {
-//            properties.load(inputStream);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private static final String DB_URL = properties.getProperty("JDBC_URL");
-//    private static final String DB_USER = properties.getProperty("JDBC_USER");
-//    private static final String DB_PASSWORD = properties.getProperty("JDBC_PASS");
+    static {
+        try (InputStream inputStream = Conexion.class.getClassLoader().getResourceAsStream("./config/config.properties")) {
+            properties.load(inputStream);
+       } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String DB_URL = properties.getProperty("JDBC_URL");
+    private static final String DB_USER = properties.getProperty("JDBC_USER");
+    private static final String DB_PASSWORD = properties.getProperty("JDBC_PASS");
 
     public static Connection getConnection() throws SQLException {
         Connection conn = null;
         try {
             Class.forName(DRIVER);
-//            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            conn = DriverManager.getConnection(connectionString);
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+//            conn = DriverManager.getConnection(connectionString);
         } catch (SQLException e) {
             System.err.println("Error al conectar con la base de datos: " + e.getMessage());
             throw e;
