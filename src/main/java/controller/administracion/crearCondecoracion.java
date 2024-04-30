52af2041c78466d2fbf95c3392212e9c428af516
Carlos Alberto Diaz B <carlos.alberto.diaz@estudiantat.upc.edu>
2024-04-30 20:54:12 +0200

    correcion crearCondecoracion servlet

--- src/main/java/controller/administracion/crearCondecoracion.java
+++ src/main/java/controller/administracion/crearCondecoracion.java
@@ -27,51 +27,51 @@ import static utilidad.PasswordManager.hashPassword;
 
 @WebServlet(name = "crearCondecoracion", urlPatterns = {"/crearCondecoracion"})
 public class crearCondecoracion extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-                response.setContentType("text/html;charset=UTF-8");
-                HttpSession session = request.getSession(false);
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession(false);
 
-                if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
-                        request.setAttribute("username", session.getAttribute("username"));
-                        request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
-                        response.setContentType("text/html;charset=UTF-8");
-                        RequestDispatcher rd = request.getRequestDispatcher("/view/administracion/crearCondecoracion.jsp");
-                        rd.forward(request, response);
-                } else {
-                    throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
-                } 
-            }
+        if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
+            request.setAttribute("username", session.getAttribute("username"));
+            request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
+            response.setContentType("text/html;charset=UTF-8");
+            RequestDispatcher rd = request.getRequestDispatcher("/view/administracion/crearCondecoracion.jsp");
+            rd.forward(request, response);
+        } else {
+            throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-                response.setContentType("text/html;charset=UTF-8");
-                HttpSession session = request.getSession(false);
-                if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
-                    request.setAttribute("username", session.getAttribute("username"));
-                    request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
-                    response.setContentType("text/html;charset=UTF-8");
-                    RequestDispatcher rd;                 
-                    String titulo = request.getParameter("titulo");
-                    String imagen = request.getParameter("imagen");
-                    String descripcion = request.getParameter("descripcion");
-                    CondecoracionDAO condecoracionDAO = new CondecoracionDAO();
-                    try{
-                        CondecoracionDTO nuevaCondecoracion = new CondecoracionDTO(titulo, imagen, descripcion);
-                        condecoracionDAO.insert(nuevaCondecoracion);
-                        
-                    }catch(SQLException e){
-                        e.getStackTrace();
-                    }
-                    request.setAttribute("titulo", titulo);
-                    rd = request.getRequestDispatcher("/view/administracion/condecoracionSubida.jsp");
-                    rd.forward(request, response);
-                } else {
-                    throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
-                } 
-                
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
+            request.setAttribute("username", session.getAttribute("username"));
+            request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
+            response.setContentType("text/html;charset=UTF-8");
+            RequestDispatcher rd;
+            String titulo = request.getParameter("titulo");
+            String imagen = request.getParameter("imagen");
+            String descripcion = request.getParameter("descripcion");
+            CondecoracionDAO condecoracionDAO = new CondecoracionDAO();
+            try {
+                CondecoracionDTO nuevaCondecoracion = new CondecoracionDTO(titulo, descripcion, imagen);
+                condecoracionDAO.insert(nuevaCondecoracion);
+
+            } catch (SQLException e) {
+                e.getStackTrace();
+            }
+            request.setAttribute("titulo", titulo);
+            rd = request.getRequestDispatcher("/view/administracion/condecoracionSubida.jsp");
+            rd.forward(request, response);
+        } else {
+            throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
+        }
+
     }
 }
