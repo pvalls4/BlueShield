c0b765c3e68a8e2e660c9ffe8979e4f2b2b4bdf0
Carlos Alberto Diaz B <carlos.alberto.diaz@estudiantat.upc.edu>
2024-05-01 20:17:06 +0200

    conexion con azure

--- src/main/java/DAO/Conexion.java
+++ src/main/java/DAO/Conexion.java
@@ -1,38 +1,21 @@
 package DAO;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Conexion {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-//    private static final String connectionString = System.getenv("AZURE_MYSQL_CONNECTIONSTRING");
-    private static final Properties properties = new Properties();
-
-    static {
-        try (InputStream inputStream = Conexion.class.getClassLoader().getResourceAsStream("./config/config.properties")) {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static final String DB_URL = properties.getProperty("JDBC_URL");
-    private static final String DB_USER = properties.getProperty("JDBC_USER");
-    private static final String DB_PASSWORD = properties.getProperty("JDBC_PASS");
+    private static final String connectionString = System.getenv("AZURE_MYSQL_CONNECTIONSTRING");
 
     public static Connection getConnection() throws SQLException {
         Connection conn = null;
         try {
             Class.forName(DRIVER);
-            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-//            conn = DriverManager.getConnection(connectionString);
+            conn = DriverManager.getConnection(connectionString);
         } catch (SQLException e) {
             throw e;
         } catch (ClassNotFoundException ex) {
