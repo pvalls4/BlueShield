496db727d43783b816ff26080a56cf755f8857db
ebrugat <xxbrugatxx@gmail.com>
2024-05-01 14:08:56 +0200

    hacer header responsive

--- src/main/java/controller/administracion/crearModelo.java
+++ src/main/java/controller/administracion/crearModelo.java
@@ -25,52 +25,53 @@ import static utilidad.PasswordManager.hashPassword;
 
 @WebServlet(name = "crearModelo", urlPatterns = {"/crearModelo"})
 public class crearModelo extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-                response.setContentType("text/html;charset=UTF-8");
-                HttpSession session = request.getSession(false);
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession(false);
 
-                if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
-                        request.setAttribute("username", session.getAttribute("username"));
-                        request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
-                        response.setContentType("text/html;charset=UTF-8");
-                        RequestDispatcher rd = request.getRequestDispatcher("/view/administracion/crearModelo.jsp");
-                        rd.forward(request, response);
-                } else {
-                    throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
-                } 
-            }
+        if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
+            request.setAttribute("username", session.getAttribute("username"));
+            request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
+            response.setContentType("text/html;charset=UTF-8");
+            RequestDispatcher rd = request.getRequestDispatcher("/view/administracion/crearModelo.jsp");
+            rd.forward(request, response);
+        } else {
+            throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-                response.setContentType("text/html;charset=UTF-8");
-                HttpSession session = request.getSession(false);
-                if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
-                    request.setAttribute("username", session.getAttribute("username"));
-                    request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
-                    response.setContentType("text/html;charset=UTF-8");
-                    RequestDispatcher rd;                 
-                    String marca = request.getParameter("marca");
-                    String modelo = request.getParameter("modelo");
-                    String imagen = request.getParameter("imagen");
-                    ModeloDAO modeloDao = new ModeloDAO();
-                    try{
-                        ModeloDTO nuevoModelo = new ModeloDTO(marca, modelo, imagen);
-                        modeloDao.insert(nuevoModelo);
-                        
-                    }catch(SQLException e){
-                        e.getStackTrace();
-                    }
-                    request.setAttribute("marca", marca);
-                    request.setAttribute("modelo", modelo);
-                    rd = request.getRequestDispatcher("/view/administracion/modeloSubido.jsp");
-                    rd.forward(request, response);
-                } else {
-                    throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
-                } 
-                
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("username") != null && (Boolean) session.getAttribute("isAdmin") == true) {
+            request.setAttribute("username", session.getAttribute("username"));
+            request.setAttribute("isAdmin", session.getAttribute("isAdmin"));
+            response.setContentType("text/html;charset=UTF-8");
+            RequestDispatcher rd;
+            String marca = request.getParameter("marca");
+            String modelo = request.getParameter("modelo");
+            String imagen = request.getParameter("imagen");
+            ModeloDAO modeloDao = new ModeloDAO();
+            try {
+                ModeloDTO nuevoModelo = new ModeloDTO(marca, modelo, imagen);
+                modeloDao.insert(nuevoModelo);
+
+            } catch (SQLException e) {
+                e.getStackTrace();
+            }
+            request.setAttribute("marca", marca);
+            request.setAttribute("modelo", modelo);
+            rd = request.getRequestDispatcher("/view/administracion/modeloSubido.jsp");
+            rd.forward(request, response);
+        } else {
+            throw new WebApplicationException("Access is forbidden to this resource", Response.Status.FORBIDDEN);
+        }
+
     }
 }
